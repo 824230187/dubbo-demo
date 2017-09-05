b85fad202a9c3f883ae78e1ec2e8a8d0c5fbd8b5
Creator <824230187@qq.com>
2017-09-05 10:40:04 +0800

    项目上传测试

---
+++ dubbo-demo-provider/src/main/java/top/jf/demo/service/DemoUserService.java
@@ -0,0 +1,47 @@
+/*
+ * www.jinvovo.com Inc.
+ * Copyright (c) 2017 All Rights Reserved
+ */
+package top.jf.demo.service;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import top.jf.demo.api.DemoUserApi;
+import top.jf.demo.core.result.SimpleResult;
+import top.jf.demo.entity.UserEntity;
+
+import java.util.Date;
+
+/**
+ * Created by IntelliJ IDEA
+ * 〈类详细描述〉 <p>
+ * 〈功能详细描述〉
+ *
+ * @author liujf
+ * @date 2017/6/1
+ * @time 10:48
+ */
+@Component
+public class DemoUserService implements DemoUserApi {
+	
+	
+	
+	public String hello (String username) {
+		String content = "Hello!This is my first dubbo demo!----"+new Date ()+"-----"+username ;
+		return content;
+	}
+	
+	public SimpleResult insertUser () {
+		SimpleResult result = new SimpleResult ();
+		result.setMessage ("系统错误");
+		result.setStatus ("Fail");
+		result.setCode ("-1");
+		
+		return result;
+	}
+	
+	public static void main (String[] args) {
+		SimpleResult result = new DemoUserService ().insertUser ();
+		System.out.println (result);
+	}
+}
